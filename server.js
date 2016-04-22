f2bcde394237947441ce17778a88f0e5aa1eb15e
Todd Hancock <thancock20@hotmail.com>
2016-04-22 12:46:55 -0500

    Add index.html

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello world!');
+  res.sendFile(process.cwd() + '/index.html');
 });
 
 app.listen(3000, function() {
