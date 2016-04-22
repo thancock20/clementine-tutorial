5e139fabb7efa835ade8e24b24d0bd4b49b17ad0
Todd Hancock <thancock20@hotmail.com>
2016-04-22 13:05:15 -0500

    Edit index.html

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var routes = require('./app/routes/index');
 
 var app = express();
 
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+
 routes(app);
 
 app.listen(3000, function() {
