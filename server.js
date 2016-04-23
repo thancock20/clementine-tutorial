2590f07fb11627d8f24d49dbff9acb093d5f1085
Todd Hancock <thancock20@hotmail.com>
2016-04-23 00:51:12 -0500

    Connect to MongoDB

--- server.js
+++ server.js
@@ -2,14 +2,24 @@
 
 var express = require('express');
 var routes = require('./app/routes/index');
+var mongo = require('mongodb').MongoClient;
 
 var app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
+  if (err) {
+    throw new Error('Database failed to connect!');
+  } else {
+    console.log('MongoDB successfully connected on port 27017.');
+  }
 
-routes(app);
 
-app.listen(3000, function() {
-  console.log('Listening on port 3000');
+  app.use('/public', express.static(process.cwd() + '/public'));
+  app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+
+  routes(app, db);
+
+  app.listen(3000, function() {
+    console.log('Listening on port 3000');
+  });
 });
