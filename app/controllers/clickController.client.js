1e950475ca92fb997d98e744f171cfbc491b68a9
Todd Hancock <thancock20@hotmail.com>
2016-04-27 12:47:35 -0500

    Add functions to Client side controller

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -5,4 +5,34 @@
     var deleteButton = document.querySelector('.btn-delete');
     var clickNbr = document.querySelector('#click-nbr');
     var apiUrl = 'http://localhost:3000/api/clicks';
+
+    function ready(fn) {
+      if (typeof fn !== 'function') {
+        return;
+      }
+
+      if (document.readyState === 'complete') {
+        return fn();
+      }
+
+      document.addEventListener('DOMContentLoaded', fn, false);
+    }
+
+    function ajaxRequest(method, url, callback) {
+      var xmlhttp = new XMLHttpRequest();
+
+      xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+          callback(xmlhttp.response);
+        }
+      };
+
+      xmlhttp.open(method, url, true);
+      xmlhttp.send();
+    }
+
+    function updateClickCount(data) {
+      var clicksObject = JSON.parse(data);
+      clickNbr.innerHTML = clicksObject.clicks;
+    }
 })();
